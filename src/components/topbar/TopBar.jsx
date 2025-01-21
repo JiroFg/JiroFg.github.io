310a1f4c340f9b7f45a06be659b0802dc594a379
edsonfuentesdev <mechcrromancer@gmail.com>
2025-01-21 14:00:15 -0800

    [FEAT] Adición de burger menu, responsividad en la top bar y cambio de fuente

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,20 +1,41 @@
 import ThemeToggle from "../ThemeToggle";
 import { Link } from 'react-router'
 import './TopBar.css'
+import { useEffect, useState } from "react";
 
 export default function TopBar({isDark, setIsDark, toPage}){
+
+    const [isOpen, setIsOpen] = useState(true)
+
+    useEffect(()=>{
+        window.addEventListener("resize", ()=>{
+            if(screen.width > 768){
+                setIsOpen(true)
+            }else{
+                setIsOpen(false)
+            }
+        })
+    }, [])
+
+    function handleMenuBtn(){
+        setIsOpen((isOpen)=>!isOpen)
+    }
+
     return(
         <div className="top-bar">
-            <div>
+            <div className="logo-top-bar">
                 Logo
+                <img src={isDark ? "menu_icon_dark.svg" : "/menu_icon.svg"} alt="Menu" onClick={handleMenuBtn} />
             </div>
-            <div className="options-top-bar">
-                <Link className="top-bar-link" to='/' onClick={toPage('home')}>Home</Link>
-                <Link className="top-bar-link" to='/projects' onClick={toPage('projects')}>Projects</Link>
-                <Link className="top-bar-link" to='/contact' onClick={toPage('contact')}>Contact</Link>
-                <Link className="top-bar-link" to='/about' onClick={toPage('about')}>About this site</Link>
-                <ThemeToggle isDark={isDark} setIsDark={setIsDark}/>
-            </div>
+            { isOpen &&
+                (<div className="options-top-bar">
+                    <Link className="top-bar-link" to='/' onClick={toPage('home')}>Home</Link>
+                    <Link className="top-bar-link" to='/projects' onClick={toPage('projects')}>Projects</Link>
+                    <Link className="top-bar-link" to='/contact' onClick={toPage('contact')}>Contact</Link>
+                    <Link className="top-bar-link" to='/about' onClick={toPage('about')}>About this site</Link>
+                    <ThemeToggle isDark={isDark} setIsDark={setIsDark}/>
+                </div>)
+            }
         </div>
     )
 }
